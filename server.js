6b69e3ca038e531492a755e092dc98949ac94d1c
GuilhermeRizzottoLis <guilherme.sbroglio@pucrs.br>
2020-02-20 14:05:29 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const requiredir = require('require-dir')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true })
+
+requiredir('./src/models')
+
+
+
+app.use('/api', require('./src/routes'))
+
+app.listen(3001)
